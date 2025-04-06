5d232587daf10974c47ec38b2dfc5c89d0bbd167
Ragow <89248406+userjimmy@users.noreply.github.com>
2025-04-06 14:17:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -46,23 +46,33 @@ function loadFoldersForTab(tabId) {
     const foldersContainer = document.getElementById(`${tabId}-folders`);
     foldersContainer.innerHTML = '';
     
-    // Create different number of folders based on tab
-    let folderCount = 0;
-    if (tabId === 'notes') {
-        folderCount = 6; // NOTES has 6 folders
-    } else if (tabId === 'cats') {
-        folderCount = 5; // CATs has 5 folders
-    } else {
-        folderCount = 5; // ASSIGNMENTS has 5 folders
-    }
+    // Create 6 folders for all tabs
+    const folderCount = 6;
     
     // Create folder elements
     for (let i = 1; i <= folderCount; i++) {
         const folder = document.createElement('div');
         folder.className = 'folder';
+        
+        // Customize folder names based on tab
+        let folderName;
+        switch(tabId) {
+            case 'notes':
+                folderName = `Unit ${i}`;
+                break;
+            case 'cats':
+                folderName = `CAT ${i}`;
+                break;
+            case 'assignments':
+                folderName = `Assignment ${i}`;
+                break;
+            default:
+                folderName = `Folder ${i}`;
+        }
+        
         folder.innerHTML = `
             <i class="fas fa-folder"></i>
-            <div class="folder-name">${tabId === 'notes' ? `Unit ${i}` : `Folder ${i}`}</div>
+            <div class="folder-name">${folderName}</div>
         `;
         
         folder.addEventListener('click', () => {
@@ -83,20 +93,48 @@ function loadFilesFromGitHub(tabId, folderNumber) {
     filesContainer.style.display = 'block';
     foldersContainer.style.display = 'none';
     
-    // Sample PDF files - in real app these would come from GitHub API
-    const sampleFiles = [
-        { name: `${tabId === 'notes' ? 'Lecture' : 'Test'} ${folderNumber}-1.pdf`, 
-          url: `https://github.com/${githubConfig.username}/${githubConfig.repo}/blob/${githubConfig.branch}/${githubConfig.folders[tabId]}/folder${folderNumber}/file1.pdf` },
-        { name: `${tabId === 'notes' ? 'Lecture' : 'Test'} ${folderNumber}-2.pdf`, 
-          url: `https://github.com/${githubConfig.username}/${githubConfig.repo}/blob/${githubConfig.branch}/${githubConfig.folders[tabId]}/folder${folderNumber}/file2.pdf` },
-        { name: `${tabId === 'notes' ? 'Summary' : 'Solution'} ${folderNumber}.pdf`, 
-          url: `https://github.com/${githubConfig.username}/${githubConfig.repo}/blob/${githubConfig.branch}/${githubConfig.folders[tabId]}/folder${folderNumber}/summary.pdf` }
-    ];
+    // Sample PDF files - customize based on tab type
+    let sampleFiles = [];
+    
+    switch(tabId) {
+        case 'notes':
+            sampleFiles = [
+                { name: `Lecture ${folderNumber}.pdf`, 
+                  url: `https://github.com/${githubConfig.username}/${githubConfig.repo}/blob/${githubConfig.branch}/${githubConfig.folders.notes}/unit${folderNumber}/lecture.pdf` },
+                { name: `Summary ${folderNumber}.pdf`, 
+                  url: `https://github.com/${githubConfig.username}/${githubConfig.repo}/blob/${githubConfig.branch}/${githubConfig.folders.notes}/unit${folderNumber}/summary.pdf` },
+                { name: `Exercises ${folderNumber}.pdf`, 
+                  url: `https://github.com/${githubConfig.username}/${githubConfig.repo}/blob/${githubConfig.branch}/${githubConfig.folders.notes}/unit${folderNumber}/exercises.pdf` }
+            ];
+            break;
+            
+        case 'cats':
+            sampleFiles = [
+                { name: `CAT ${folderNumber} Questions.pdf`, 
+                  url: `https://github.com/${githubConfig.username}/${githubConfig.repo}/blob/${githubConfig.branch}/${githubConfig.folders.cats}/cat${folderNumber}/questions.pdf` },
+                { name: `CAT ${folderNumber} Solutions.pdf`, 
+                  url: `https://github.com/${githubConfig.username}/${githubConfig.repo}/blob/${githubConfig.branch}/${githubConfig.folders.cats}/cat${folderNumber}/solutions.pdf` },
+                { name: `CAT ${folderNumber} Feedback.pdf`, 
+                  url: `https://github.com/${githubConfig.username}/${githubConfig.repo}/blob/${githubConfig.branch}/${githubConfig.folders.cats}/cat${folderNumber}/feedback.pdf` }
+            ];
+            break;
+            
+        case 'assignments':
+            sampleFiles = [
+                { name: `Assignment ${folderNumber}.pdf`, 
+                  url: `https://github.com/${githubConfig.username}/${githubConfig.repo}/blob/${githubConfig.branch}/${githubConfig.folders.assignments}/assignment${folderNumber}/task.pdf` },
+                { name: `Solution ${folderNumber}.pdf`, 
+                  url: `https://github.com/${githubConfig.username}/${githubConfig.repo}/blob/${githubConfig.branch}/${githubConfig.folders.assignments}/assignment${folderNumber}/solution.pdf` },
+                { name: `Rubric ${folderNumber}.pdf`, 
+                  url: `https://github.com/${githubConfig.username}/${githubConfig.repo}/blob/${githubConfig.branch}/${githubConfig.folders.assignments}/assignment${folderNumber}/rubric.pdf` }
+            ];
+            break;
+    }
     
     // Create back button
     filesContainer.innerHTML = `
         <div class="back-button" id="back-to-folders">
-            <i class="fas fa-arrow-left"></i> Back to folders
+            <i class="fas fa-arrow-left"></i> Back to ${tabId}
         </div>
     `;
     
